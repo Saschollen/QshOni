1c2d621642eea5fd0c41b31ddc06190e6f6f38be
Scott Schollenberger <89707936+Saschollen@users.noreply.github.com>
2025-07-05 22:50:02 -0400

    QSHPYRUN initial help (incomplete)

--- .conflict-base-0/qshpython/pystdoutstderrortest1.py
+++
@@ -1,6 +0,0 @@
-# Sample to write to STDOUT and STDERROR
-# Both streams seem to write to console
-import sys
-
-sys.stdout.write( "Hello Standard Output!\n" )
-sys.stderr.write( "Hello Standard Error!\n" )
